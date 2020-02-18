15672c39b51bdb16bc27b3136525030d029f913d
Sarin Upreti <sarin.upreti01@gmail.com>
2020-02-19 00:06:56 +0545

    implemented firebase fcm token registration

--- lib/index.dart
+++ lib/index.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:steve_beaudoin/routes/routes.dart';
 import 'package:steve_beaudoin/screens/home.dart';
@@ -12,7 +13,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
   @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print("FCM_TOKEN : $token");
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
